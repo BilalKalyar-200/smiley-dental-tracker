d81813ef5b7206d77c3b3e68d5fade4603d5d329
BilalKalyar-200 <mohsinkalyarpk@gmail.com>
2026-04-24 12:23:25 +0500

    initial project setup with MERN structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 text-white flex items-center justify-center text-3xl font-bold">
+      Tailwind Working
+    </div>
+  );
+}
+
+export default App;
